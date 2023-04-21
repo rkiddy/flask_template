2e26161e46f5304c32fcb93f40349ce9c5e2d2ca
Ray Kiddy <ray@ganymede.org>
2023-04-21 10:38:06 -0700

    More working better.

--- app.py
+++ app.py
@@ -17,8 +17,11 @@ env = Environment(loader=PackageLoader('app', 'pages'))
 
 @app.route(f"/{cfg['WWW']}")
 @app.route(f"/{cfg['WWW']}/")
-def contracts_main():
-    main = env.get_template('app.html')
+def app_main():
+    main = env.get_template('app_main.html')
     context = data.app_main()
     return main.render(**context)
+
+if __name__ == '__main__':
+    app.run()
 
